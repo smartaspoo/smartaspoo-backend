0acb7f1befec8027b8f6d7e3a254f57c434bbc8f
Abdul Aziz <112202006541@mhs.dinus.ac.id>
2023-06-26 18:56:30 +0700

    bikin penjualan _children, dan diskon

--- database/migrations/2023_06_26_054600_create_penjualan_children_table.php
+++ database/migrations/2023_06_26_054600_create_penjualan_children_table.php
@@ -15,13 +15,15 @@ return new class extends Migration
     {
         Schema::create('penjualan_children', function (Blueprint $table) {
             $table->id();
-            $table->integer('penjualan_children');
             $table->string('nomor_faktur');
-            $table->foreign('nomor_faktur')->references("nomor_faktur")->on("penjualan");
+            $table->foreign('nomor_faktur')->references('nomor_faktur')->on('penjualan');
             $table->string('kode_barang');
-            $table->foreign('kode_barang')->references("kode_barang")->on("barang");
-            $table->string('kode_diskon');
-            $table->foreign('kode_diskon')->references("nomor_faktur")->on("penjualan");
+            $table->foreign('kode_barang')->references('kode_barang')->on('barang');
+            $table->bigInteger('kode_diskon');
+            $table->foreign('kode_diskon')->references('kode_diskon')->on('diskon');
+            $table->integer('harga_jual');
+            $table->integer('jumlah_item');
+            $table->integer('total_bayar');
             $table->timestamps();
         });
     }
