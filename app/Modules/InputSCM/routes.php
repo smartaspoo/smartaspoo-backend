d9a50cb8aa589ca83f8c093a629e8b6b38976886
Hanif Setyananda <hanifsetya@dinustek.com>
2023-10-23 22:59:47 +0700

    aneh

--- app/Modules/InputSCM/routes.php
+++ app/Modules/InputSCM/routes.php
@@ -15,73 +15,73 @@ Route::prefix('/input-scm')->group(function() {
     // SUB MENU MARKER (DONT DELETE THIS LINE)
 
     Route::get("/excel",[InputSCMController::class,'excel']);
-    Route::get('/', [InputSCMController::class, 'index'])->middleware('authorize:read-input_scm');
-    Route::get('/datatable', [InputSCMController::class, 'datatable'])->middleware('authorize:read-input_scm');
-    Route::get('/create', [InputSCMController::class, 'create'])->middleware('authorize:create-input_scm');
-    Route::post('/', [InputSCMController::class, 'store'])->middleware('authorize:create-input_scm');
-    Route::get('/{input_scm_id}', [InputSCMController::class, 'show'])->middleware('authorize:read-input_scm');
-    Route::get('/{input_scm_id}/edit', [InputSCMController::class, 'edit'])->middleware('authorize:update-input_scm');
-    Route::post('/{input_scm_id}/edit', [InputSCMController::class, 'getEdit'])->middleware('authorize:update-input_scm');
-    Route::post('/{input_scm_id}/edit/save', [InputSCMController::class, 'saveEdit'])->middleware('authorize:update-input_scm');
+    Route::get('/', [InputSCMController::class, 'index']);
+    Route::get('/datatable', [InputSCMController::class, 'datatable']);
+    Route::get('/create', [InputSCMController::class, 'create']);
+    Route::post('/', [InputSCMController::class, 'store']);
+    Route::get('/{input_scm_id}', [InputSCMController::class, 'show']);
+    Route::get('/{input_scm_id}/edit', [InputSCMController::class, 'edit']);
+    Route::post('/{input_scm_id}/edit', [InputSCMController::class, 'getEdit']);
+    Route::post('/{input_scm_id}/edit/save', [InputSCMController::class, 'saveEdit']);
 
-    Route::put('/{input_scm_id}', [InputSCMController::class, 'update'])->middleware('authorize:update-input_scm');
-    Route::delete('/{input_scm_id}', [InputSCMController::class, 'destroy'])->middleware('authorize:delete-input_scm');
+    Route::put('/{input_scm_id}', [InputSCMController::class, 'update']);
+    Route::delete('/{input_scm_id}', [InputSCMController::class, 'destroy']);
 
 
     Route::prefix("/{id_umkm}")->group(function(){
-        Route::get('/preview', [InputSCMController::class, 'preview'])->middleware('authorize:read-input_scm');
+        Route::get('/preview', [InputSCMController::class, 'preview']);
 
         Route::prefix("/barang")->group(function(){
-            Route::get('/', [BarangController::class, 'index'])->middleware('authorize:read-input_scm');
-            Route::get('/datatable', [BarangController::class, 'datatable'])->middleware('authorize:read-input_scm');
-            Route::get('/create', [BarangController::class, 'create'])->middleware('authorize:create-input_scm');
-            Route::post('/create', [BarangController::class, 'store'])->middleware('authorize:create-input_scm');
-            Route::get('/{id_barang}', [BarangController::class, 'show'])->middleware('authorize:read-input_scm');
-            Route::get('/{id_barang}/edit', [BarangController::class, 'edit'])->middleware('authorize:update-input_scm');
-            Route::post('/{id_barang}/edit', [BarangController::class, 'getEdit'])->middleware('authorize:update-input_scm');
-            Route::post('/{id_barang}/editsave', [BarangController::class, 'saveEdit'])->middleware('authorize:update-input_scm');
+            Route::get('/', [BarangController::class, 'index']);
+            Route::get('/datatable', [BarangController::class, 'datatable']);
+            Route::get('/create', [BarangController::class, 'create']);
+            Route::post('/create', [BarangController::class, 'store']);
+            Route::get('/{id_barang}', [BarangController::class, 'show']);
+            Route::get('/{id_barang}/edit', [BarangController::class, 'edit']);
+            Route::post('/{id_barang}/edit', [BarangController::class, 'getEdit']);
+            Route::post('/{id_barang}/editsave', [BarangController::class, 'saveEdit']);
             
-            Route::put('/{id_barang}', [BarangController::class, 'update'])->middleware('authorize:update-input_scm');
-            Route::delete('/{id_barang}', [BarangController::class, 'destroy'])->middleware('authorize:delete-input_scm');
+            Route::put('/{id_barang}', [BarangController::class, 'update']);
+            Route::delete('/{id_barang}', [BarangController::class, 'destroy']);
         });
     });
 
     
     Route::prefix("/bahan")->group(function(){
             Route::prefix("/{id_barang}")->group(function(){
-            Route::get('/', [BahanController::class, 'index'])->middleware('authorize:read-input_scm');
-            Route::get('/datatable', [BahanController::class, 'datatable'])->middleware('authorize:read-input_scm');
-            Route::get('/create', [BahanController::class, 'create'])->middleware('authorize:create-input_scm');
-            Route::get('/create/datatable', [BahanController::class,'datatableSupplier'])->middleware('authorize:read-input_scm');
-            Route::post('/create', [BahanController::class, 'store'])->middleware('authoriz e:create-input_scm');
-            Route::get('/{id_bahan}', [BahanController::class, 'show'])->middleware('authorize:read-input_scm');
-            Route::get('/{id_bahan}/edit', [BahanController::class, 'edit'])->middleware('authorize:update-input_scm');
-            Route::post('/{id_bahan}/edit', [BahanController::class, 'getEdit'])->middleware('authorize:update-input_scm');
-            Route::post('/{id_bahan}/edit/save', [BahanController::class, 'saveEdit'])->middleware('authorize:update-input_scm');
+            Route::get('/', [BahanController::class, 'index']);
+            Route::get('/datatable', [BahanController::class, 'datatable']);
+            Route::get('/create', [BahanController::class, 'create']);
+            Route::get('/create/datatable', [BahanController::class,'datatableSupplier']);
+            Route::post('/create', [BahanController::class, 'store']);
+            Route::get('/{id_bahan}', [BahanController::class, 'show']);
+            Route::get('/{id_bahan}/edit', [BahanController::class, 'edit']);
+            Route::post('/{id_bahan}/edit', [BahanController::class, 'getEdit']);
+            Route::post('/{id_bahan}/edit/save', [BahanController::class, 'saveEdit']);
             
-            Route::put('/{id_bahan}', [BahanController::class, 'update'])->middleware('authorize:update-input_scm');
-            Route::delete('/{id_bahan}', [BahanController::class, 'destroy'])->middleware('authorize:delete-input_scm');
+            Route::put('/{id_bahan}', [BahanController::class, 'update']);
+            Route::delete('/{id_bahan}', [BahanController::class, 'destroy']);
         });
     });
      Route::prefix("/supplier")->group(function(){
         Route::prefix("/{id_bahan}")->group(function(){
-        Route::get('/', [SupplierController::class, 'index'])->middleware('authorize:read-input_scm');
-        Route::get('/datatable', [SupplierController::class, 'datatable'])->middleware('authorize:read-input_scm');
-        Route::get('/create', [SupplierController::class, 'create'])->middleware('authorize:create-input_scm');
-        Route::post('/create', [SupplierController::class, 'store'])->middleware('authorize:create-input_scm');
-        Route::get('/{id_supplier}', [SupplierController::class, 'show'])->middleware('authorize:read-input_scm');
-        Route::get('/{id_supplier}/edit', [SupplierController::class, 'edit'])->middleware('authorize:update-input_scm');
-        Route::put('/{id_supplier}', [SupplierController::class, 'update'])->middleware('authorize:update-input_scm');
-        Route::delete('/{id_supplier}', [SupplierController::class, 'destroy'])->middleware('authorize:delete-input_scm');
+        Route::get('/', [SupplierController::class, 'index']);
+        Route::get('/datatable', [SupplierController::class, 'datatable']);
+        Route::get('/create', [SupplierController::class, 'create']);
+        Route::post('/create', [SupplierController::class, 'store']);
+        Route::get('/{id_supplier}', [SupplierController::class, 'show']);
+        Route::get('/{id_supplier}/edit', [SupplierController::class, 'edit']);
+        Route::put('/{id_supplier}', [SupplierController::class, 'update']);
+        Route::delete('/{id_supplier}', [SupplierController::class, 'destroy']);
         });
      });
     
   
 
     Route::prefix('/alamat')->group(function() {
-        Route::get('/provinsi', [AlamatController::class, 'provinsi'])->middleware('authorize:read-input_scm');
-        Route::get('/kota/{id}', [AlamatController::class, 'kota'])->middleware('authorize:read-input_scm');
-        Route::get('/kecamatan/{id}', [AlamatController::class, 'kecamatan'])->middleware('authorize:read-input_scm');
-        Route::get('/kelurahan/{id}', [AlamatController::class, 'kelurahan'])->middleware('authorize:read-input_scm');
+        Route::get('/provinsi', [AlamatController::class, 'provinsi']);
+        Route::get('/kota/{id}', [AlamatController::class, 'kota']);
+        Route::get('/kecamatan/{id}', [AlamatController::class, 'kecamatan']);
+        Route::get('/kelurahan/{id}', [AlamatController::class, 'kelurahan']);
     });
 });
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\KaryawanController;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use App\Modules\InputSCM\Controllers\AlamatController;
+use App\Modules\InputSCM\Controllers\BahanController;
+use App\Modules\InputSCM\Controllers\BarangController;
+use App\Modules\InputSCM\Controllers\InputSCMController;
+use App\Modules\InputSCM\Controllers\SupplierController;
 
 /*
 |--------------------------------------------------------------------------
@@ -55,3 +60,79 @@ Route::get('/', function () {
 Route::get('/p/order', [OrderController::class, 'order']);
 Route::post('/bayar', [OrderController::class, 'bayar']);
 Route::get('/p/invoice/{id}', [OrderController::class, 'invoice']);
+
+Route::prefix('/input-scm')->group(function() {
+
+    // SUB MENU MARKER (DONT DELETE THIS LINE)
+
+    Route::get("/excel",[InputSCMController::class,'excel']);
+    Route::get('/', [InputSCMController::class, 'index']);
+    Route::get('/datatable', [InputSCMController::class, 'datatable']);
+    Route::get('/create', [InputSCMController::class, 'create']);
+    Route::post('/', [InputSCMController::class, 'store']);
+    Route::get('/{input_scm_id}', [InputSCMController::class, 'show']);
+    Route::get('/{input_scm_id}/edit', [InputSCMController::class, 'edit']);
+    Route::post('/{input_scm_id}/edit', [InputSCMController::class, 'getEdit']);
+    Route::post('/{input_scm_id}/edit/save', [InputSCMController::class, 'saveEdit']);
+
+    Route::put('/{input_scm_id}', [InputSCMController::class, 'update']);
+    Route::delete('/{input_scm_id}', [InputSCMController::class, 'destroy']);
+
+
+    Route::prefix("/{id_umkm}")->group(function(){
+        Route::get('/preview', [InputSCMController::class, 'preview']);
+
+        Route::prefix("/barang")->group(function(){
+            Route::get('/', [BarangController::class, 'index']);
+            Route::get('/datatable', [BarangController::class, 'datatable']);
+            Route::get('/create', [BarangController::class, 'create']);
+            Route::post('/create', [BarangController::class, 'store']);
+            Route::get('/{id_barang}', [BarangController::class, 'show']);
+            Route::get('/{id_barang}/edit', [BarangController::class, 'edit']);
+            Route::post('/{id_barang}/edit', [BarangController::class, 'getEdit']);
+            Route::post('/{id_barang}/editsave', [BarangController::class, 'saveEdit']);
+            
+            Route::put('/{id_barang}', [BarangController::class, 'update']);
+            Route::delete('/{id_barang}', [BarangController::class, 'destroy']);
+        });
+    });
+
+    
+    Route::prefix("/bahan")->group(function(){
+            Route::prefix("/{id_barang}")->group(function(){
+            Route::get('/', [BahanController::class, 'index']);
+            Route::get('/datatable', [BahanController::class, 'datatable']);
+            Route::get('/create', [BahanController::class, 'create']);
+            Route::get('/create/datatable', [BahanController::class,'datatableSupplier']);
+            Route::post('/create', [BahanController::class, 'store']);
+            Route::get('/{id_bahan}', [BahanController::class, 'show']);
+            Route::get('/{id_bahan}/edit', [BahanController::class, 'edit']);
+            Route::post('/{id_bahan}/edit', [BahanController::class, 'getEdit']);
+            Route::post('/{id_bahan}/edit/save', [BahanController::class, 'saveEdit']);
+            
+            Route::put('/{id_bahan}', [BahanController::class, 'update']);
+            Route::delete('/{id_bahan}', [BahanController::class, 'destroy']);
+        });
+    });
+     Route::prefix("/supplier")->group(function(){
+        Route::prefix("/{id_bahan}")->group(function(){
+        Route::get('/', [SupplierController::class, 'index']);
+        Route::get('/datatable', [SupplierController::class, 'datatable']);
+        Route::get('/create', [SupplierController::class, 'create']);
+        Route::post('/create', [SupplierController::class, 'store']);
+        Route::get('/{id_supplier}', [SupplierController::class, 'show']);
+        Route::get('/{id_supplier}/edit', [SupplierController::class, 'edit']);
+        Route::put('/{id_supplier}', [SupplierController::class, 'update']);
+        Route::delete('/{id_supplier}', [SupplierController::class, 'destroy']);
+        });
+     });
+    
+  
+
+    Route::prefix('/alamat')->group(function() {
+        Route::get('/provinsi', [AlamatController::class, 'provinsi']);
+        Route::get('/kota/{id}', [AlamatController::class, 'kota']);
+        Route::get('/kecamatan/{id}', [AlamatController::class, 'kecamatan']);
+        Route::get('/kelurahan/{id}', [AlamatController::class, 'kelurahan']);
+    });
+});
